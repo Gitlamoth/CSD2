f3b2a4980eb954345bbff73e4f11d93d9d73e834
Bram <bram.laman@outlook.com>
2023-11-16 01:14:35 +0100

    replaced C++2b file to main CSD

---
+++ C++2b/Instrument Class/instrument.h
@@ -0,0 +1,19 @@
+#include <iostream>
+
+//So called Header file for resembling available variables
+
+class Instrument {
+public:
+    float String;
+    float Body;
+    float Hertz;
+
+
+    Instrument(float string, float body);
+
+    void radiation();
+
+    void hz();
+    
+};
+
